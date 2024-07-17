bc2d5a2d5858b93a76284c14013411568ed7a645
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-07-18 08:49:25 +0900

    feat : GoogleloginRequest 수정

--- src/main/java/com/ddoddii/resume/dto/UserGoogleLoginRequestDTO.java
+++ src/main/java/com/ddoddii/resume/dto/UserGoogleLoginRequestDTO.java
@@ -4,20 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @Setter
 @Builder
-<<<<<<<< HEAD:src/main/java/com/ddoddii/resume/dto/UserGoogleLoginRequestDTO.java
 public class UserGoogleLoginRequestDTO {
     private String idToken;
     private String name;
-    private String email;
-========
-public class UserAuthResponseDTO {
-    private UserDTO user;
-    private JwtTokenDTO token;
->>>>>>>> feature/login:src/main/java/com/ddoddii/resume/dto/UserAuthResponseDTO.java
 }
+
