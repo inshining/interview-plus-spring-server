e9f59889f39285dc33ac8e80e7eded7e32a83395
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-07-18 20:33:17 +0900

    feat : company 정보 DB 에서 가져오

---
+++ src/main/java/com/ddoddii/resume/dto/company/CompanyNameDTO.java
@@ -0,0 +1,14 @@
+package com.ddoddii.resume.dto.company;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CompanyNameDTO {
+    private String companyName;
+}
