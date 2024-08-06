e84f5b65655f942f182dfac5445af098c9fa027e
inshining <inshining.kim@gmail.com>
2024-08-06 18:23:49 +0900

    ➕[ADD] : test package 추가

---
+++ src/test/java/com/ddoddii/resume/integration/TempTest.java
@@ -0,0 +1,14 @@
+package com.ddoddii.resume.integration;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TempTest {
+    
+    @Test
+    void test(){
+        int a = 1;
+        int b = 2;
+        Assertions.assertEquals(3, a+b);
+    }
+}
