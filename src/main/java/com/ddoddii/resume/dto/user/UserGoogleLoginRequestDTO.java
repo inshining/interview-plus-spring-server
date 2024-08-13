33f28aaefdfe28fc2e6592b701de317c366a077f
inshining <inshining.kim@gmail.com>
2024-08-13 17:56:15 +0900

    ➕[ADD] : UserGoogleLoginRequestDTO에 Valid 추가

--- src/main/java/com/ddoddii/resume/dto/user/UserGoogleLoginRequestDTO.java
+++ src/main/java/com/ddoddii/resume/dto/user/UserGoogleLoginRequestDTO.java
@@ -1,5 +1,7 @@
 package com.ddoddii.resume.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,8 +12,13 @@ import lombok.Setter;
 @Setter
 @Builder
 public class UserGoogleLoginRequestDTO {
+
+    @NotBlank
     private String idToken;
     private String name;
+
+    @Email
+    @NotBlank
     private String email;
 }
 
