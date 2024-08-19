43bb3257b88513db83a1e97857784a813d7c3dc5
inshining <inshining.kim@gmail.com>
2024-08-19 19:30:03 +0900

    ➕[ADD] : sql 문 추가

---
+++ src/test/resources/sql/company_dept.sql
@@ -0,0 +1,18 @@
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (8, 'HR');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (17, '건설·시설');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (9, '게임 제작');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (1, '경영·비즈니스');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (5, '고객서비스·리테일');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (18, '공공·복지');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (12, '교육');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (10, '금융');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (3, '디자인');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (2, '마케팅·광고');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (14, '물류·무역');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (6, '미디어');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (15, '법률·법집행기관');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (16, '식·음료');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (7, '엔지니어링·설계');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (4, '영업');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (13, '의료·제약·바이오');
+INSERT INTO `resume-plus`.company_dept (id, dept) VALUES (11, '제조·생산');
